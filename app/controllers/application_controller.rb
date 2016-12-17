b34402fc0392baab213a8e9682ee2cb4bfe76d07
Gabe da Silveira <gabe@websaviour.com>
2016-12-17 13:49:35 -0800

    Poor man's user accounts
    Accounts live and die by browser session.  Pretty janky, but good for
    a quick demo.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :load_user
+
+  # Poor-mans user account for demo purposes, it just lives as long as your
+  # session does, and guarantees we always have a user.
+  def load_user
+    if session[:user_id]
+      @current_user = User.where(id: session[:user_id]).first
+    end
+
+    if !@current_user
+      @current_user = User.create
+      session[:user_id] = @current_user.id
+    end
+  end
+
+  def current_user
+    @current_user
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    render text: current_user.id
   end
 end
